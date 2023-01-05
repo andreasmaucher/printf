6c8fd234c8cba9b4acbacc6536643bec2614936b
Andreas Maucher <amaucher@c4b10c1.42berlin.de>
2023-01-05 10:48:04 +0100

    created_files

---
+++ printf.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printf.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amaucher <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/05 10:40:11 by amaucher          #+#    #+#             */
+/*   Updated: 2023/01/05 10:40:15 by amaucher         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PRINTF_H
+# define PRINTF_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <strings.h>
+#include <stdlib.h>
+
+void	ft_putstr(char *str);
+void	ft_putchar(char c);
+void	ft_putnbr(int nb);
+
+#endif
